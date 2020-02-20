60684981496e7ea26d05b3e5595ca51c5df9f3c7
Natalia <47737431+elline777@users.noreply.github.com>
2020-02-20 05:12:58 +0500

    Create and delete new pages on server

---
+++ app/src/main.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Editor from './components/editor';
+
+ReactDOM.render(<Editor />, document.getElementById('root'));
+
+// function getPageList() {
+//   $('h1').remove();
+//   $.get(
+//     './api',
+//     data => {
+//       data.forEach(file => {
+//         $('body').append(`<h1>${file}</h1>`);
+//       });
+//     },
+//     'JSON'
+//   );
+// }
+
+// getPageList();
+
+// $('button').click(() => {
+//   $.post(
+//     './api/createNewPage.php',
+//     {
+//       name: $('input').val()
+//     },
+//     () => {
+//       getPageList();
+//     }
+//   ).fail(() => {
+//     alert('Страница уже существует!');
+//   });
+// });
