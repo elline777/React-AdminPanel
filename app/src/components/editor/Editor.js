8c9731474c5f86b8abd9a942f407697853a42ae5
Natalia <47737431+elline777@users.noreply.github.com>
2020-02-26 18:15:43 +0500

    Add contentEditable to text nodes

--- app/src/components/editor/Editor.js
+++ app/src/components/editor/Editor.js
@@ -1,9 +1,12 @@
+import '../../helpers/iframeLoader';
 import axios from 'axios';
 import React, { Component } from 'react';
 
 export default class Editor extends Component {
   constructor() {
     super();
+
+    this.currentPage = 'index.html';
 
     this.state = {
       pageList: [],
@@ -13,7 +16,87 @@ export default class Editor extends Component {
   }
 
   componentDidMount() {
+    this.init(this.currentPage);
+  }
+
+  init(page) {
+    this.iframe = document.querySelector('iframe');
+    this.open(page);
     this.loadPageList();
+  }
+
+  open(page) {
+    this.currentPage = `../${page}?rnd=${Math.random()}`;
+
+    axios
+      .get(`../${page}`)
+      .then(res => this.parseStrToDom(res.data))
+      .then(this.wrapTextNodes)
+      .then(dom => {
+        this.virtualDom = dom;
+        return dom;
+      })
+      .then(this.serializeDOMToString)
+      .then(html => axios.post('./api/saveTempPage.php', { html }))
+      .then(() => this.iframe.load('../temp.html'))
+      .then(() => this.enableEditing());
+  }
+
+  enableEditing() {
+    this.iframe.contentDocument.body
+      .querySelectorAll('text-editor')
+      .forEach(element => {
+        g;
+        element.contentEditable = 'true';
+        element.addEventListener('input', () => {
+          this.onTextEdit(element);
+        });
+      });
+  }
+
+  onTextEdit(element) {
+    const id = element.getAttribute('nodeid');
+    this.virtualDom.body.querySelector(`[nodeid="${id}"]`).innerHTML =
+      element.innerHTML;
+  }
+
+  parseStrToDom(str) {
+    const parser = new DOMParser();
+    return parser.parseFromString(str, 'text/html');
+  }
+
+  wrapTextNodes(dom) {
+    const body = dom.body;
+    let textNodes = [];
+
+    function recursy(element) {
+      element.childNodes.forEach(node => {
+        if (
+          node.nodeName === '#text' &&
+          node.nodeValue.replace(/\s+/g, '').length > 0
+        ) {
+          textNodes.push(node);
+        } else {
+          recursy(node);
+        }
+      });
+    }
+
+    recursy(body);
+
+    textNodes.forEach((node, i) => {
+      const wrapper = dom.createElement('text-editor');
+      node.parentNode.replaceChild(wrapper, node);
+      wrapper.appendChild(node);
+      wrapper.setAttribute('nodeid', i);
+    });
+
+    return dom;
+  }
+
+  serializeDOMToString(dom) {
+    const serializer = new XMLSerializer();
+    return serializer.serializeToString(dom);
   }
 
   loadPageList() {
@@ -43,27 +126,28 @@ export default class Editor extends Component {
   }
 
   render() {
-    const { pageList } = this.state;
-    const pages = pageList.map((page, i) => {
-      return (
-        <h1 key={i}>
-          {page}
-          <a href="#" onClick={() => this.deletePage(page)}>
-            (x)
-          </a>
-        </h1>
-      );
-    });
+    // const { pageList } = this.state;
+    // const pages = pageList.map((page, i) => {
+    //   return (
+    //     <h1 key={i}>
+    //       {page}
+    //       <a href="#" onClick={() => this.deletePage(page)}>
+    //         (x)
+    //       </a>
+    //     </h1>
+    //   );
+    // });
 
     return (
-      <>
-        <input
-          onChange={e => this.setState({ newPageName: e.target.value })}
-          type="text"
-        />
-        <button onClick={this.createNewPage}>Создать страницу</button>
-        {pages}
-      </>
+      <iframe src={this.currentPage} frameBorder="0"></iframe>
+      // <>
+      //   <input
+      //     onChange={e => this.setState({ newPageName: e.target.value })}
+      //     type="text"
+      //   />
+      //   <button onClick={this.createNewPage}>Создать страницу</button>
+      //   {pages}
+      // </>
     );
   }
 }
